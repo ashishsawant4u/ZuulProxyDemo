ad2c4f03df544830e8c773685c158d33ee0f53b0
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-08-03 21:53:29 +0530

    Creating Zuul Proxy for PriceMS

--- src/main/java/com/boot/demo/ZuulProxyDemoApplication.java
+++ src/main/java/com/boot/demo/ZuulProxyDemoApplication.java
@@ -2,7 +2,9 @@ package com.boot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulProxyDemoApplication {
 
